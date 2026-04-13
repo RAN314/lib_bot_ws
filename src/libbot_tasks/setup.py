b1f89fc54f21b261fcaf8f3f3a25b8641bb8c8b2
lihaolan <1412046028@qq.com>
2026-04-13 13:46:56 +0800

    chore: epic2配置文件

--- src/libbot_tasks/setup.py
+++ src/libbot_tasks/setup.py
@@ -4,7 +4,7 @@ package_name = 'libbot_tasks'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='lhl',
-    maintainer_email='1412046028@qq.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='开发者',
+    maintainer_email='developer@example.com',
+    description='图书馆机器人任务包 - L1恢复行为实现',
+    license='Apache License 2.0',
     extras_require={
         'test': [
             'pytest',
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'l1_recovery_node = libbot_tasks.l1_recovery_node:main',
         ],
     },
 )
