b4f058f9cd9f9a38228187b5d05241b2cf14e5e1
lihaolan <1412046028@qq.com>
2026-04-20 20:24:20 +0800

    feat: 将manipulation的turtlebot集成进

---
+++ src/libbot_simulation/launch/library_nav2_use_sim_time.launch.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+#
+# 图书馆机器人Nav2导航仿真启动文件
+# 基于turtlebot3_manipulation_navigation2/launch/navigation2_use_sim_time.launch.py修改
+# 使用aws-robomaker-bookstore-world场景和地图
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    start_rviz = LaunchConfiguration('start_rviz')
+    use_sim = LaunchConfiguration('use_sim')
+    map_yaml_file = LaunchConfiguration('map_yaml_file')
+    params_file = LaunchConfiguration('params_file')
+    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
+    autostart = LaunchConfiguration('autostart')
+    use_composition = LaunchConfiguration('use_composition')
+    use_respawn = LaunchConfiguration('use_respawn')
+
+    map_yaml_file = LaunchConfiguration(
+        'map_yaml_file',
+        default=PathJoinSubstitution(
+            [
+                FindPackageShare('aws_robomaker_bookstore_world'),
+                'maps',
+                'turtlebot3_waffle_pi',
+                'map.yaml'
+            ]
+        )
+    )
+
+    params_file = LaunchConfiguration(
+        'params_file',
+        default=PathJoinSubstitution(
+            [
+                FindPackageShare('nav2_bringup'),
+                'params',
+                'nav2_params.yaml'
+            ]
+        )
+    )
+
+    nav2_launch_file_dir = PathJoinSubstitution(
+        [
+            FindPackageShare('nav2_bringup'),
+            'launch',
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare('nav2_bringup'),
+            'rviz',
+            'nav2_default_view.rviz'
+        ]
+    )
+
+    default_bt_xml_filename = PathJoinSubstitution(
+        [
+            FindPackageShare('nav2_bt_navigator'),
+            'behavior_trees',
+            'navigate_w_replanning_and_recovery.xml'
+        ]
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'start_rviz',
+            default_value='true',
+            description='Whether execute rviz2'),
+
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='true',
+            description='Start robot in Gazebo simulation'),
+
+        DeclareLaunchArgument(
+            'map_yaml_file',
+            default_value=map_yaml_file,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=params_file,
+            description='Full path to the ROS2 parameters file to use for all launched nodes'),
+
+        DeclareLaunchArgument(
+            'default_bt_xml_filename',
+            default_value=default_bt_xml_filename,
+            description='Full path to the behavior tree xml file to use'),
+
+        DeclareLaunchArgument(
+            'autostart',
+            default_value='true',
+            description='Automatically startup the nav2 stack'),
+
+        DeclareLaunchArgument(
+            'use_composition',
+            default_value='True',
+            description='Whether to use composed bringup'),
+
+        DeclareLaunchArgument(
+            'use_respawn',
+            default_value='false',
+            description='Whether to respawn if a node crashes. \
+                Applied when composition is disabled.'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_yaml_file,
+                'use_sim_time': use_sim,
+                'params_file': params_file,
+                'default_bt_xml_filename': default_bt_xml_filename,
+                'autostart': autostart,
+                'use_composition': use_composition,
+                'use_respawn': use_respawn,
+            }.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen',
+            condition=IfCondition(start_rviz)),
+    ])
